a76897808e4adc9c9b104dda9db720f97f1b795c
hff213 <1316143551@qq.com>
2020-06-27 20:18:32 +0800

    'new'

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import toast from 'components/common/toast'
+import LazyLoad from 'vue-lazyload'
+Vue.use(LazyLoad,{loading:require('./assets/img/common/placeholder.png')})
 Vue.prototype.$bus=new Vue()
 Vue.config.productionTip = false
 Vue.use(toast)
